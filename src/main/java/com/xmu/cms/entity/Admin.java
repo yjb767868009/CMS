ccd41856aece20d6577d7266bf61719353da340b
余炬波 <767868009@qq.com>
2018-11-23 20:27:07 +0800

    entity init

---
+++ src/main/java/com/xmu/cms/entity/Admin.java
@@ -0,0 +1,9 @@
+package com.xmu.cms.entity;
+
+/**
+ * @author JuboYu on 2018/11/23.
+ * @version 1.0
+ */
+public class Admin {
+
+}
---
+++ src/main/java/com/xmu/cms/entity/Teacher.java
@@ -0,0 +1,8 @@
+package com.xmu.cms.entity;
+
+/**
+ * @author JuboYu on 2018/11/23.
+ * @version 1.0
+ */
+public class Teacher {
+}

7469f5a7a46299252b86a02833a2e213553982ba
wuwei <992964285@qq.com>
2018-11-23 18:42:00 +0800

    vue init

---
+++ frontend/src/components/CmsHeader.vue
@@ -0,0 +1,54 @@
+<template>
+    <header style="background-color: rgb(10, 47, 88);">
+        <div class="text-logo">{{msg}}</div>
+        <p class="meta">{{intro}}</p>
+        <ul class="header-operations">
+        </ul>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'db-header',
+        data () {
+            return {
+                msg: 'CMS',
+                intro:'Manager'
+            }
+        }
+    }
+</script>
+
+<style>
+    header {
+        height: 70px;
+        position: absolute;
+        width: 100%;
+        top: 0;
+        left: 0;
+        padding: 0 20px;
+        z-index: 1;
+        box-sizing: border-box;
+    }
+
+    .text-logo {
+        display: inline-block;
+        vertical-align: middle;
+        border-style: none;
+        position: relative;
+        top: 26px;
+        right: -20px;
+        font-size: 25px;
+        color: white;
+
+    }
+
+    .meta {
+        color: #7e95c5;
+        width: 200px;
+        display: block;
+        margin: -5px 0 0 225px;
+        font-weight: 700;
+        font-size: 0.3rem;
+    }
+</style>
---
+++ frontend/src/components/CmsSidebar.vue
@@ -0,0 +1,25 @@
+<template>
+<ul class="el-meun">
+    <li class="el-menu-item is-active" style="">管理教师信息</li>
+    <li class="el-menu-item" style="">管理学生信息</li>
+</ul>    
+</template>
+
+<script>
+
+    export default {
+        name: 'db-sidebar',
+        data() {
+            return {
+                msg: 'sidebar'
+            }
+        }
+    }
+
+</script>
+
+<style>
+    li {
+        padding-left: 20px;
+    }
+</style>

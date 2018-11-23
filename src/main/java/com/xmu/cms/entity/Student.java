f551da414e19dfdd3a1d67506c6ec3e0995ac1c1
余炬波 <767868009@qq.com>
2018-11-23 20:58:19 +0800

    student entity init

--- src/main/java/com/xmu/cms/entity/Student.java
+++ src/main/java/com/xmu/cms/entity/Student.java
@@ -5,4 +5,67 @@ package com.xmu.cms.entity;
  * @version 1.0
  */
 public class Student {
+
+    private String id;
+    private String account;
+    private String password;
+    private String email;
+    private String name;
+    private boolean activation;
+    private int messageInterval;
+
+    public Student(String id, String account, String password, String name) {
+        this.id = id;
+        this.account = account;
+        this.password = password;
+        this.name = name;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isActivation() {
+        return activation;
+    }
+
+    public void setActivation(boolean activation) {
+        this.activation = activation;
+    }
+
+    public int getMessageInterval() {
+        return messageInterval;
+    }
+
+    public void setMessageInterval(int messageInterval) {
+        this.messageInterval = messageInterval;
+    }
 }

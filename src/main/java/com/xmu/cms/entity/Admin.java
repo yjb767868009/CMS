51d0d80ccd0ea30557fd9ddfd34dbc7a3996c2fd
余炬波 <767868009@qq.com>
2018-11-23 20:58:04 +0800

    admin entity init

--- src/main/java/com/xmu/cms/entity/Admin.java
+++ src/main/java/com/xmu/cms/entity/Admin.java
@@ -5,5 +5,22 @@ package com.xmu.cms.entity;
  * @version 1.0
  */
 public class Admin {
+    private String account;
+    private String password;
 
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }

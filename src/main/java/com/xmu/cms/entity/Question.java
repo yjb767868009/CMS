16dcf0ab37dca1dd6d919f8a658e505c290edf3c
zhangjingzheng <wx_c4400347c1274d629f845c69bdb851f9@git.code.tencent.com>
2018-11-24 08:54:47 +0000

    score

--- src/main/java/com/xmu/cms/entity/Question.java
+++ src/main/java/com/xmu/cms/entity/Question.java
@@ -5,4 +5,29 @@ package com.xmu.cms.entity;
  * @version 1.0
  */
 public class Question {
+    private Long id;
+    private boolean selected;
+    private double score;
+    public Question(){
+        selected=false;
+        score=0;
+    }
+    public void setID(Long id){
+        this.id=id;
+    }
+    public Long getID(){
+        return this.id;
+    }
+    public void isSelected(){
+        selected=true;
+    }
+    public void notSelected(){
+        selected=false;
+    }
+    public void setScore(double score){
+        this.score=score;
+    }
+    public double get_score(){
+        return this.score;
+    }
 }

35d8d403154c2d4bf262beaf5a1d52967b0dc3fb
余炬波 <767868009@qq.com>
2018-11-25 16:29:42 +0800

    update admin controller api

--- src/main/java/com/xmu/cms/controller/AdminController.java
+++ src/main/java/com/xmu/cms/controller/AdminController.java
@@ -1,5 +1,7 @@
 package com.xmu.cms.controller;
 
+import com.xmu.cms.dao.StudentDao;
+import com.xmu.cms.dao.TeacherDao;
 import com.xmu.cms.entity.Student;
 import com.xmu.cms.service.StudentService;
 import com.xmu.cms.service.TeacherService;
@@ -22,49 +24,83 @@ import java.util.List;
 public class AdminController {
 
     @Autowired
-    private StudentService studentService;
+    private StudentDao studentDao;
 
     @Autowired
-    private TeacherService teacherService;
+    private TeacherDao teacherDao;
+
+    @Autowired
+    private AdminDao adminDao;
 
     @PostMapping(value = "/login")
-    public Admin adminLogIn(@ModelAttribute Admin admin){
-        return admin;
+    public String adminLogIn(@RequestParam(value = "account") String account,
+                             @RequestParam(value = "password") String password) {
+        String findPassword = adminDao.getPasswordByAccount(account);
+        if (findPassword == null) {
+            return "No this account";
+        } else {
+            if (password.equals(findPassword)) {
+                return "Success";
+            } else {
+                return "Account or Password error";
+            }
+        }
     }
 
     @GetMapping(value = "/teachers")
-    public List<Teacher> getAllTeachers(){
-        return teacherService.getAllTeachers();
+    public List<Teacher> getAllTeachers() {
+        return teacherDao.getAllTeachers();
     }
 
     @GetMapping(value = "/students")
-    public List<Student> getAllStudents(){
-        return studentService.getAllStudents();
+    public List<Student> getAllStudents() {
+        return studentDao.getAllStudents();
     }
 
     @PostMapping(value = "/teacher")
-    public String newTeacher(@ModelAttribute Teacher teacher){
-        return teacherService.newTeacher(teacher);
+    public String newTeacher(@RequestParam(value = "name") String name,
+                             @RequestParam(value = "account") String account,
+                             @RequestParam(value = "password") String password,
+                             @RequestParam(value = "email") String email) {
+        return teacherDao.newTeacher(teacher);
     }
 
-    @PutMapping(value = "/teacher")
-    public Teacher modifyTeacher(@ModelAttribute Teacher teacher){
-        return  teacher;
+    @PutMapping(value = "/teacher/{teacherId}")
+    public String modifyTeacherById(@PathVariable("teacherId") Long teacherId,
+                                    @RequestParam(value = "name") String name,
+                                    @RequestParam(value = "account") String account,
+                                    @RequestParam(value = "email") String email) {
+        return teacherDao.modifyTeacherById(teacherId, name, account, email);
     }
 
-    @DeleteMapping(value = "/teacher")
-    public Teacher deleteTeacher(@ModelAttribute Teacher teacher){
-        return teacher;
+    @PatchMapping(value = "/teacher/{teacherId}/modifyPassword")
+    public String modifyTeacherPasswordById(@PathVariable("teacherId") Long teacherId,
+                                            @RequestParam(value = "password") String password) {
+        return teacherDao.modifyTeacherPasswordById(teacherId, password);
     }
 
-    @PutMapping(value = "/student")
-    public Student modifyStudent(@ModelAttribute Student student){
-        return student;
+    @DeleteMapping(value = "/teacher/{teacherId}")
+    public String deleteTeacher(@PathVariable("teacherId") Long teacherId) {
+        return teacherDao.deleteTeacherById(teacherId);
     }
 
-    @DeleteMapping(value = "/student")
-    public Student deleteStudent(@ModelAttribute Student student){
-        return student;
+    @PutMapping(value = "/student/{studentId}")
+    public String modifyStudent(@PathVariable("studentId") Long studentId,
+                                 @RequestParam("name") String name,
+                                 @RequestParam("account") String account,
+                                 @RequestParam("email") String email) {
+        return studentDao.modifyStudentById(studentId,name,account,email);
+    }
+
+    @PatchMapping(value = "student/{studentId}/modifyPassword")
+    public String modifyStudentPasswordById(@PathVariable("studentId") Long studentId,
+                                            @RequestParam(value = "password") String password){
+        return studentDao.modifyStudentPasswordById(studentId,password);
+    }
+
+    @DeleteMapping(value = "/student/{studentId}")
+    public String deleteStudent(@PathVariable("studentId") Long studentId) {
+        return studentDao.deleteStudentById(studentId);
     }
 
 }

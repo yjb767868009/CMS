58bf88fbec1fb14262820792a4f46262692881d6
余炬波 <767868009@qq.com>
2018-11-29 00:25:09 +0800

    add sessions and redis

--- src/main/java/com/xmu/cms/CmsApplication.java
+++ src/main/java/com/xmu/cms/CmsApplication.java
@@ -2,7 +2,11 @@ package com.xmu.cms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+@EnableCaching
+@EnableRedisHttpSession
 @SpringBootApplication
 public class CmsApplication {
 

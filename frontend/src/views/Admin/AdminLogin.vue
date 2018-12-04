2a4c66ff0a170d68d8ab4d5173f1cd3b92a192be
wuwe1 <992964285@qq.com>
2018-12-04 13:38:05 +0800

    delete test button

--- frontend/src/views/Admin/AdminLogin.vue
+++ frontend/src/views/Admin/AdminLogin.vue
@@ -6,7 +6,6 @@
         <el-input v-model="account" placeholder="账户名"></el-input>
         <el-input type="password" v-model="password" placeholder="密码" ></el-input>
         <el-button style="width:100%" @click="adminLogin(account,password)">登陆</el-button>
-        <el-button style="width:100%" @click="toAdmin">test</el-button>
     </el-main>
 </el-container>
 

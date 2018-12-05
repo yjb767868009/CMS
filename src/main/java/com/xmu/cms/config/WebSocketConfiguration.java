da1962e7ebfd1eb995dc5ff28ececa2bb7ab5b8e
余炬波 <767868009@qq.com>
2018-12-05 15:57:51 +0800

    web socket init

---
+++ src/main/java/com/xmu/cms/config/WebSocketConfiguration.java
@@ -0,0 +1,26 @@
+package com.xmu.cms.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/**
+ * @author JuboYu on 2018/12/5.
+ * @version 1.0
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/gs-guide-websocket").withSockJS();
+    }
+}

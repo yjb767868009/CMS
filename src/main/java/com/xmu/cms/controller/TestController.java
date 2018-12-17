b880c71e86f0c0456f4efbe17dc6ae1d801f0908
余炬波 <767868009@qq.com>
2018-12-17 20:03:34 +0800

    update

--- src/main/java/com/xmu/cms/controller/TestController.java
+++ src/main/java/com/xmu/cms/controller/TestController.java
@@ -1,11 +1,7 @@
 package com.xmu.cms.controller;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.Claim;
-import com.auth0.jwt.interfaces.DecodedJWT;
-import com.xmu.cms.support.Token;
+import com.xmu.cms.support.Greeting;
+import com.xmu.cms.support.HelloMessage;
 import com.xmu.cms.support.UserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,15 +9,16 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.util.HtmlUtils;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -29,42 +26,14 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Map;
 
 /**
  * @author JuboYu on 2018/11/27.
  * @version 1.0
  */
 @RestController
-@RequestMapping(value = "/api/test")
+@RequestMapping(value = "/")
 public class TestController {
-    private static final String SECRET = "JKKLJOoasdlfj";
-    @Autowired
-    private JavaMailSender mailSender;
-
-    @Value("${spring.mail.username}")
-    private String sender;
-
-    @GetMapping(value = "/testSession")
-    public String testSession(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
-        HttpSession session = httpServletRequest.getSession();
-        if (session.getAttribute("userType") == null) {
-            session.setAttribute("userType", "student");
-            return "No user login";
-        } else {
-            return session.getAttribute("userType").toString();
-        }
-
-    }
-
-    @GetMapping(value = "/getJWT")
-    public UserInfo getJWT(HttpServletRequest request) {
-        return null;
-    }
-
-    @GetMapping(value = "/createJWT")
-    public void testJWT() {
-    }
 
     @GetMapping(value = "/getUserType")
     public Object getUserType(HttpSession session) {
@@ -72,7 +41,7 @@ public class TestController {
     }
 
     @GetMapping(value = "/getUser")
-    public Object getUserId(HttpSession session) {
+    public Object getUserId() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         System.out.println(authentication.toString());
         if (!(authentication instanceof AnonymousAuthenticationToken)) {
@@ -81,30 +50,11 @@ public class TestController {
         return null;
     }
 
-    private static String UPLOADED_FOLDER = "D://temp//";
-
-    @PostMapping("/uploadFile")
-    public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file) {
-        if (file.isEmpty()) {
-            return new ResponseEntity<>("please select a file!", HttpStatus.OK);
-        }
-        try {
-            byte[] bytes = file.getBytes();
-            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
-            Files.write(path, bytes);
-        } catch (IOException e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-        return new ResponseEntity<>("Successfully uploaded - " +
-                file.getOriginalFilename(), new HttpHeaders(), HttpStatus.OK);
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
     }
-
-    @GetMapping(value = "/getSecurity")
-    public Object getSecurity() {
-        SecurityContext securityContext = SecurityContextHolder.getContext();
-        Authentication auth = securityContext.getAuthentication();
-        return auth;
-    }
-
 
 }

353c2791dfef3833aa5894363f7ba53979022e9a
wuwei <992964285@qq.com>
2018-12-25 21:50:25 +0800

    update

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -8,9 +8,10 @@ import teacher_store from './teacher/teacher_store'
 export default new Vuex.Store({
     modules:{
         admin:admin_store,
-        teacher:teacher_store
+        teacher:teacher_store,
+        student:student_store,
     },
-    state:{
+    state:{//this.$store.state.teacher
         show:false,
         token:'',
         userType:'',
--- frontend/src/views/Teacher/mobile/ManageCourse.vue
+++ frontend/src/views/Teacher/mobile/ManageCourse.vue
@@ -1,106 +1,167 @@
 <template>
   <div class="course" style="background:#eee">
-    <x-header title="课程管理" style="height:60px;padding-top:12px" :left-options="{showBack:false}" :right-options="{showMore: true}" @on-click-more="show=!show">
+    <x-header title="课程管理" style="height:60px;padding-top:12px" :left-options="{showBack:false}" :right-options="{showMore: true}"
+      @on-click-more="show=!show">
     </x-header>
-    <group :title="课程">
-      <cell
-      title="OOAD（主）"
-      is-link
-      :border-intent="false"
-      :arrow-direction="showContent001 ? 'up' : 'down'"
-      @click.native="showContent001 = !showContent001"></cell>
 
-        <template v-if="showContent001">
-            <cell-box :border-intent="false" class="sub-item" is-link link="">学生成绩</cell-box>
-            <cell-box class="sub-item" is-link link="">学生组队</cell-box>
-            <cell-box class="sub-item" is-link link="">课程信息</cell-box>
-            <cell-box class="sub-item" is-link link="">班级信息</cell-box>
-            <cell-box class="sub-item" is-link link="">讨论课设置</cell-box>
-            <cell-box class="sub-item" is-link link="">共享设置</cell-box>
+    <group>
+      <template v-for="course in courses">
+        <cell :key="course.courseId" :title="course.courseName" is-link :border-intent="false" :arrow-direction="course.showCourseContent?'up':'down'"
+          @click.native="course.showCourseContent=!course.showCourseContent">
+        </cell>
+        <template v-if="course.showCourseContent">
+          <div :key="course.teacher.email" style="margin-left:20px">
+          <cell-box :border-intent="false" class="sub-item" is-link @click.native="classInfo(course)">学生成绩</cell-box>
+          <cell-box class="sub-item" is-link @click.native="classInfo(course)">学生组队</cell-box>
+          <cell-box class="sub-item" is-link @click.native="classInfo(course)">课程信息</cell-box>
+          <cell-box class="sub-item" is-link @click.native="classInfo(course)">班级信息</cell-box>
+          <cell-box class="sub-item" is-link @click.native="classInfo(course)">讨论课设置</cell-box>
+          <cell-box class="sub-item" is-link @click.native="classInfo(course)">共享设置</cell-box>
+          </div>
         </template>
-
-      <cell
-      title="J2EE"
-      is-link
-      :border-intent="false"
-      :arrow-direction="showContent002 ? 'up' : 'down'"
-      @click.native="showContent002 = !showContent002"></cell>
-
-        <template v-if="showContent002">
-            <cell-box :border-intent="false" class="sub-item" is-link link="">学生成绩</cell-box>
-            <cell-box class="sub-item" is-link link="">学生组队</cell-box>
-            <cell-box class="sub-item" is-link link="">课程信息</cell-box>
-            <cell-box class="sub-item" is-link link="">班级信息</cell-box>
-            <cell-box class="sub-item" is-link link="/mobile/teacher/setseminar">讨论课设置</cell-box>
-            <cell-box class="sub-item" is-link link=""> 共享设置</cell-box>
-        </template>
-
-      <cell title="新建课程" link="/mobile/teacher/createcourse"></cell>
+      </template>
     </group>
+    <br/>
+    <x-button  type="primary" @click.native="newCourse">新建课程</x-button>
+
+
+
     <div v-transfer-dom>
       <popup v-model="show" height="23%">
-          <div>
-              <cell value-align="left" title=""><img slot="icon" src="@/assets/message.png" style="display:block;margin-right:10px;" width="30px" height="30px"/><div style="padding-left:110px;font-size:1.3em;color:#000" @click="Undo">代办</div></cell>
-              <cell value-align="left" title=""><img slot="icon" src="@/assets/man.png" style="display:block;margin-right:10px;" width="30px" height="30px"/><div style="padding-left:110px;font-size:1.3em;color:#000" @click="TeacherInfo">个人页</div></cell>
-              <cell value-align="left" title=""><img slot="icon" src="@/assets/book.png" style="display:block;margin-right:10px;" width="30px" height="30px"/><div style="padding-left:110px;font-size:1.3em;color:#000" @click="GoSeminar">讨论课</div></cell>
-          </div>
+        <div>
+          <cell value-align="left" title=""><img slot="icon" src="@/assets/message.png" style="display:block;margin-right:10px;"
+              width="30px" height="30px" />
+            <div style="padding-left:110px;font-size:1.3em;color:#000" @click="Undo">代办</div>
+          </cell>
+          <cell value-align="left" title=""><img slot="icon" src="@/assets/man.png" style="display:block;margin-right:10px;"
+              width="30px" height="30px" />
+            <div style="padding-left:110px;font-size:1.3em;color:#000" @click="TeacherInfo">个人页</div>
+          </cell>
+          <cell value-align="left" title=""><img slot="icon" src="@/assets/book.png" style="display:block;margin-right:10px;"
+              width="30px" height="30px" />
+            <div style="padding-left:110px;font-size:1.3em;color:#000" @click="GoSeminar">讨论课</div>
+          </cell>
+        </div>
       </popup>
     </div>
   </div>
 </template>
 
 <script>
-import {XHeader,Cell,CellBox,Group,TransferDom,Popup} from 'vux'
-export default {
-  directives:{
-    TransferDom
-  },
-    components:{
-        XHeader,
-        Cell,
-        CellBox,
-        Group,Popup
+  import {
+    XHeader,
+    Cell,
+    CellBox,
+    Group,
+    TransferDom,
+    Popup,
+    XButton
+  } from 'vux'
+  export default {
+    directives: {
+      TransferDom
     },
-    data(){
-        return{
-            showContent001: false,
-            showContent002: false,
-            show:false,
-        }   
+    components: {
+      XHeader,
+      Cell,
+      CellBox,
+      Group,
+      Popup,XButton
+    },
+    data() {
+      return {
+        show: false,
+        courses: [{
+          showCourseContent: false,
+          courseId: 1,
+          teacher: {
+            teacherId: 1,
+            email: "992964285@qq.com",
+            name: "wu wei"
+          },
+          courseName: "j2ee",
+          introduction: "j2ee",
+          presentationWeight: 50,
+          reportWeight: 20,
+          questionWeight: 30,
+          teamStartTime: "2018-11-27T14:00:00.000+0000",
+          teamEndTime: "2018-12-01T14:00:00.000+0000"
+        },{
+          showCourseContent: false,
+          courseId: 2,
+          teacher: {
+            teacherId: 1,
+            email: "992964285@qq.com",
+            name: "wu wei"
+          },
+          courseName: "ooad",
+          introduction: "j2ee",
+          presentationWeight: 50,
+          reportWeight: 20,
+          questionWeight: 30,
+          teamStartTime: "2018-11-27T14:00:00.000+0000",
+          teamEndTime: "2018-12-01T14:00:00.000+0000"
+        }],
+        coursePlus: {
+          "1": "主",
+          "2": "从"
+        },
+      }
+    },
+    mounted: function () {
+      this.$axios.get('/course')
+        .then((response) => {
+          this.courses = response.data.courses
+          this.coursePlus = response.data.coursePlus
+        })
+      //为返回数据绑定show属性用于折叠显示
+      for (var i = 0; i < this.courses.length; i++) {
+        this.courses.showCourseContent = false
+      }
     },
     methods: {
-        onClick () {
-            console.log('on click')
-        },
-        back:function(){
-            this.$router.push('/mobile/teacher')
-        },
-    Undo(){
-            this.$router.push('/mobile/teacher/notify')
-        },
-    TeacherInfo(){
-            this.$router.push('/mobile/teacher')
-        },
-    GoSeminar(){
-            this.$router.push('/mobile/teacher/seminars')
-        },
+      onClick() {
+        console.log('on click')
+      },
+      back: function () {
+        this.$router.push('/mobile/teacher')
+      },
+      Undo() {
+        this.$router.push('/mobile/teacher/notify')
+      },
+      TeacherInfo() {
+        this.$router.push('/mobile/teacher')
+      },
+      GoSeminar() {
+        this.$router.push('/mobile/teacher/seminars')
+      },
+      newCourse(){
+          console.log('new course')
+      },
+      classInfo(){
+
+      }
     }
-}
+  }
+
 </script>
 
 <style scoped>
-.sub-item {
-  color: #888;
-}
-.slide {
-  padding: 0 20px;
-  overflow: hidden;
-  max-height: 0;
-  transition: max-height .5s cubic-bezier(0, 1, 0, 1) -.1s;
-}
-.animate {
-  max-height: 9999px;
-  transition-timing-function: cubic-bezier(0.5, 0, 1, 0);
-  transition-delay: 0s;
-}
+  .sub-item {
+    color: #888;
+  }
+
+  .slide {
+    padding: 0 20px;
+    overflow: hidden;
+    max-height: 0;
+    transition: max-height .5s cubic-bezier(0, 1, 0, 1) -.1s;
+  }
+
+  .animate {
+    max-height: 9999px;
+    transition-timing-function: cubic-bezier(0.5, 0, 1, 0);
+    transition-delay: 0s;
+  }
+
 </style>

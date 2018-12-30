58544bb958313bf96765c48c218c091beeed86fb
zjz <1393649219@qq.com>
2018-12-30 18:39:12 +0800

    updateError

--- frontend/src/views/Teacher/mobile/GradeList.vue
+++ frontend/src/views/Teacher/mobile/GradeList.vue
@@ -20,7 +20,7 @@
       </template> -->
 
       <template v-for="team in allteam">
-            <cell :key="team.teamId" is-link :border-intent="false" :arrow-direction="team.showTeamContent ? 'up' : 'down'"
+            <cell is-link :border-intent="false" :arrow-direction="team.showTeamContent ? 'up' : 'down'"
                 @click.native="showOneTeam(team)" value-align="left">
                 
                 <img slot="icon" src="@/assets/book.png" style="display:block;margin-right:10px;" width="30px" height="30px"/>
@@ -30,7 +30,6 @@
             <template v-if="team.showTeamContent">
         <template v-for="round in rounds">
         <cell style="height:30px"
-          :key="round.roundId"
           :title="'第'+round.order+'轮'"
           is-link
           value-align="left"
@@ -43,7 +42,7 @@
         <!-- 小组的round -->
         <template v-if="round.showRoundContent">
           <template v-for="seminar in round.seminars">
-            <cell :key="seminar.seminarId" is-link :border-intent="false" :arrow-direction="seminar.showSeminarContent ? 'up' : 'down'"
+            <cell is-link :border-intent="false" :arrow-direction="seminar.showSeminarContent ? 'up' : 'down'"
             @click.native="getSeminarScore(team,seminar,round)" value-align="left" style="padding-right:10px">
             <span>{{seminar.topic}}</span>
         </cell>
@@ -54,7 +53,7 @@
                     展示：{{score.preScpre}}<span v-if="score.preScore==null">0</span>&emsp;&emsp;&emsp;提问：{{score.questionScore}}<span v-if="score.questionScore==null">0</span>&emsp;&emsp;&emsp;书面报告：{{score.reportScore}}<span v-if="score.reportScore==null">0</span>
                     </div>
                 </cell>
-            </template>
+            </template>s
             </template>
                     <p  style="color:#000;padding-left:220px;margin-top:0">本轮成绩：{{roundscore.totalScore}}<span v-if="roundscore.totalScore==null">0</span></p>
         
@@ -154,11 +153,11 @@ export default {
                 if(this.rounds[i].showRoundContent===true&&this.rounds[i]!=Round){
                     this.rounds[i].showRoundContent=false;
                     
-                    for(var j=0;j<this.rounds[i].seminars.length;j++){
-                    if(this.rounds[i].seminars[j].showSeminarContent===true&&this.rounds[i]!=Round){
-                        this.rounds[i].seminars[j].showSeminarContent=false;
-                    }
-                }
+                    // for(var j=0;j<this.rounds[i].seminars.length;j++){
+                    // if(this.rounds[i].seminars[j].showSeminarContent===true&&this.rounds[i]!=Round){
+                    //     this.rounds[i].seminars[j].showSeminarContent=false;
+                    // }
+                // }
 
                 }
             }
